ad65703a5c69838b69942614dc87a8dd717892d0
Jon Corbin <jon.corbin8@gmail.com>
2020-09-23 12:59:12 -0400

    add some logging

--- src/lib/latency.ts
+++ src/lib/latency.ts
@@ -3,7 +3,9 @@ import { RequestPromise } from 'request-promise-native'
 export async function latency (fn: (...args: any) => RequestPromise<any[]>, ...args: any) {
   const start = Date.now()
   try {
+    console.log(`running ${fn.name} with args ${args}`)
     const result = await fn(...args)
+    console.log(`result: ${result}`)
     const latency = Date.now() - start
     return { latency, success: !!result.length }
   } catch (err) {
