5b2eb7d24fd7610f1df5368c525e276ce0f21262
Andrew Stuart <andrew.stuart2@gmail.com>
2021-11-02 10:55:56 -0700

    Add typings for output

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export class HealthZ {
     this.deps.push(new Dependency(name, fn, args))
   }
 
-  async getHealth (): Promise<any> {
+  async getHealth (): Promise<Map<string, object>> {
     const health = {} as any
     health.host = hostStats()
 
